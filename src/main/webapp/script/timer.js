47fa9a5097667ad7ae45f1eac3b3cc6ce2e29fdf
Ostap Petruniak <eliosfriend@gmail.com>
2023-01-31 18:17:51 +0200

    [QTB-2] Fixed timer bug.

--- src/main/webapp/script/timer.js
+++ src/main/webapp/script/timer.js
@@ -4,23 +4,27 @@ let currentTime = 0;
 let hour = {
     timeZone: 'Europe/Kyiv',
     hour: 'numeric',
+    hour12: false,
   },
   hours = new Intl.DateTimeFormat([], hour);
 let minute = {
     timeZone: 'Europe/Kyiv',
     minute: 'numeric',
+    hour12: false,
   },
   minutes = new Intl.DateTimeFormat([], minute);
   let second = {
       timeZone: 'Europe/Kyiv',
       second: 'numeric',
+      hour12: false,
     },
     seconds = new Intl.DateTimeFormat([], second);
 setInterval(() => {
   currentTime = parseInt(hours.format(new Date())) * 3600 + parseInt(minutes.format(new Date())) * 60 + parseInt(seconds.format(new Date()));
   difference = Math.abs(currentTime - secondsOfStart);
   document.getElementById("countDown").value = "Time left: " + Math.trunc((time-difference)/60) + ":" + (time-difference)%60;
-  if(difference >= time){
-    window.location.replace("/result/" + document.getElementById("testId").value);
-  }
+   if(difference >= time){
+      window.location.replace("/result/" + document.getElementById("testId").value);
+    }
+
 }, 1000);
